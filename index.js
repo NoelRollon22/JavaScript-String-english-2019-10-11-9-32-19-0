9adcbba0e7167bbf99e1c7f6fc5d200342ae1f97
NoelRollon22 <111257604+NoelRollon22@users.noreply.github.com>
2022-08-18 14:26:47 +0800

    Update index.js

--- index.js
+++ index.js
@@ -1,13 +1,22 @@
 // Complete the following procedure, the 'name' variable in all letters to uppercase, output: ' 'HELLO' '.
 var name = 'hello';
 // TODO write your code here...
-
+var upper = name.toUpperCase();
+console.log(upper);
 
 // Complete the following procedure, capitalizing all the words in the variable sentence and printing: 'Good Afternoon, Mr Mike.'
 var sentence = 'good afternoon, mr mike.';
 //TODO write your code here...
+const words = sentence.split(" ");
+for (let i = 0; i < words.length; i++) {
+    words[i] = words[i][0].toUpperCase() + words[i].substr(1);
+}
+console.log(words.join(" "));
 
 
 // Complete the following procedure to extract the amount of money variable, output: 20.
 var money = '￥20';
 //TODO write your code here...
+var extract = money.substring(1);
+console.log(extract)
+
